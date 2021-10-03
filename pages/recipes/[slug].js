3b99c52987596a6b2d74a02d3df69a66cfaf8070
ltk55 <franklam@live.hk>
2021-10-03 17:11:20 +0100

    Feat: Revalidate contentful every 1 sec

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -32,6 +32,7 @@ export const getStaticProps = async ({ params }) => {
 
   return {
     props: { recipe: items[0] },
+    revalidate: 1,
   };
 };
 
@@ -44,7 +45,6 @@ export default function RecipeDetails({ recipe }) {
     <div>
       <div className="banner">
         <Image
-          alt={title}
           src={"https:" + featuredImage.fields.file.url}
           width={featuredImage.fields.file.details.image.width}
           height={featuredImage.fields.file.details.image.height}
