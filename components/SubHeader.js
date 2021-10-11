a279860f1968682a133a0a675efaceb6a2472357
ltk55 <franklam@live.hk>
2021-10-11 17:16:49 +0100

    style: update SubHeader style

--- components/SubHeader.js
+++ components/SubHeader.js
@@ -1,6 +1,6 @@
 if (typeof window !== "undefined") {
   let i = 0;
-  const el = document.getElementById("background");
+  const el = document.getElementById("change");
   const color = ["#f0d87a", "#40dc67", "#489fe4", "#ec7914", "#ebc693"];
   const text = ["Macchiato", "Cappccino", "Mocha", "Dalgona", "Latte"];
 
@@ -9,15 +9,21 @@ if (typeof window !== "undefined") {
     el.innerText = text[i];
     el.style.color = color[i];
   };
-  setInterval(change, 1000);
+  setInterval(change, 2000);
 }
 
 export default function SubHeader() {
   return (
     <div>
-      <span>
-        learn to make<span id="background"></span>
-      </span>
+      <h2 className="subHeader__text">
+        Learn to make <span id="change">Espresso</span>
+      </h2>
+
+      <style jsx>{`
+        .subHeader__text {
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
