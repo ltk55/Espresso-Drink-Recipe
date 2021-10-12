5fac39904bc1879de4e40772343a664e05a846f6
Frank Lam <franlam55@gmail.com>
2021-10-12 10:49:41 +0100

    Fix: Add alt in image comp

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -8,6 +8,7 @@ export default function RecipeCard({ recipe }) {
     <div className="card">
       <div className="featured">
         <Image
+          alt={title}
           src={"https:" + thumbnail.fields.file.url}
           width={thumbnail.fields.file.details.image.width}
           height={thumbnail.fields.file.details.image.height}
